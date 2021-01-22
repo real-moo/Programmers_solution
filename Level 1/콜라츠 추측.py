8d65e1681ce4606d16bec84b4a39409e1ed1c8c1
real-moo <73583182+real-moo@users.noreply.github.com>
2021-01-22 16:16:25 +0900

    Create 콜라츠 추측.py

---
+++ Level 1/콜라츠 추측.py
@@ -0,0 +1,18 @@
+def solution(num):
+    count = 0
+    
+    while True:
+        # num이 1이 되면 작업 횟수 반환
+        if num == 1:
+            return count
+        count += 1
+        # num이 짝수일 경우, num / 2
+        if num % 2 == 0:
+            num /= 2
+        # num이 홀수일 경우, num * 3 + 1
+        else:
+            num *= 3
+            num += 1
+        # 작업을 500번 반복해도 1이 되지 않는다면 -1 반환
+        if count == 500:
+            return -1
